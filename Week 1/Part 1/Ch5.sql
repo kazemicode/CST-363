e239ebe44f3d56b575664d3550d7f3039bf4b7b9
kazemicode <sarieses@gmail.com>
2019-01-12 22:27:57 -0800

    Replace Ch5

--- Week 1/Part 1/Ch5.sql
+++ Week 1/Part 1/Ch5.sql
@@ -43,6 +43,10 @@ WHERE vendor_id IN
     FROM vendors
     WHERE default_terms_id = 2);
 
-
+-- Exercise 9
+DELETE FROM invoice_line_items
+WHERE invoice_id = 116;
+DELETE FROM invoices
+WHERE invoice_id = 116;
 
 	
